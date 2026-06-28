8b0ad12c89412b9c050e835beed32438f7fe3e6f
rajendran55764-lab <rajendran55764@gmail.com>
2026-06-28 12:51:24 +0530

    Create Cart.js

---
+++ server/models/Cart.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const CartSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+      },
+      name: String,
+      price: Number,
+      quantity: {
+        type: Number,
+        default: 1
+      },
+      image: String
+    }
+  ],
+  totalAmount: {
+    type: Number,
+    default: 0
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Cart', CartSchema);
